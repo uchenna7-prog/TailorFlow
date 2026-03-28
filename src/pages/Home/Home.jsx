6d1f356c94926e667691d4963933295deb08bd9a
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-03-28 14:56:41 +0100

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -25,10 +25,7 @@ function Home({ onMenuClick }) {
         {/* Greeting */}
         <section className={styles.heroSection}>
           <div className={styles.greetHeader}>
-            <span className={styles.greetText}>Welcome back 👋,</span>
-          </div>
-          <div className={styles.signatureWrapper}>
-            <span className={styles.signatureName}>Uchenna</span>
+            <span className={styles.greetText}>Welcome back 👋, Uchenna</span>
           </div>
           <p className={styles.greetSubText}>
             Here’s what’s happening in your tailoring shop today
