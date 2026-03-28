5dff2640ed23ddbe7dead93d80148a7bf1b5b9ef
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-03-28 06:41:31 +0100

    Create Toast/Toast.jsx

---
+++ src/components/Toast/Toast.jsx
@@ -0,0 +1,9 @@
+import styles from './Toast.module.css'
+
+export default function Toast({ message }) {
+  return (
+    <div className={`${styles.toast} ${message ? styles.show : ''}`}>
+      {message}
+    </div>
+  )
+}
