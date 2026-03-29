0cc0713b5225381ef4a66615decc685ed8105113
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-03-29 21:28:05 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { CustomerProvider } from './contexts/CustomerContext'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { CustomerProvider } from './contexts/CustomerContext';
+import { OrdersProvider } from './contexts/OrdersContext'; // ✅ import OrdersProvider
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <CustomerProvider>
-        <App />
+        <OrdersProvider> {/* ✅ wrap with OrdersProvider */}
+          <App />
+        </OrdersProvider>
       </CustomerProvider>
     </BrowserRouter>
   </StrictMode>
-)
+);
