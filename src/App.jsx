a64f70309143f112756404f42acf4033b94ef75e
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-03-29 16:06:12 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import SideBar from './components/SideBar/SideBar'
 import Home from './pages/Home/Home'
 import Customers from './pages/Customers/Customers'
 import CustomerDetail from './pages/CustomerDetail/CustomerDetail'
-
+import Tasks from './pages/Tasks/Tasks'
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary'
 
 function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false)
@@ -15,10 +16,11 @@ function App() {
       <Routes>
         <Route path="/"              element={<Home         onMenuClick={() => setSidebarOpen(true)} />} />
         <Route path="/customers"     element={<Customers    onMenuClick={() => setSidebarOpen(true)} />} />
+        <Route path="/tasks"         element={<Tasks        onMenuClick={() => setSidebarOpen(true)} />} />
         <Route path="/customers/:id" element={
-          
+          <ErrorBoundary>
             <CustomerDetail onMenuClick={() => setSidebarOpen(true)} />
-          
+          </ErrorBoundary>
         } />
       </Routes>
     </>
