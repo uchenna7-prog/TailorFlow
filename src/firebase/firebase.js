61925f5177b9902fc83b81db145fbefab3ccdd90
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-03-31 16:23:04 +0100

    Create firebase/firebase.js

---
+++ src/firebase/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// import { getStorage } from "firebase/storage"; // optional
+
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_MSG_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// Export services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+// export const storage = getStorage(app);
+
+export default app;
